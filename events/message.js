8682d6226a96a3c9b65dc149fa8efbf849d1bc26
henrique945 <henrique.rodrigues7@hotmail.com>
2020-06-25 12:30:38 -0300

    add gif comand bot

--- events/message.js
+++ events/message.js
@@ -1,6 +1,9 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+const GphApiClient = require('giphy-js-sdk-core');
+const giphy = GphApiClient(process.env.GIPHYTOKEN);
+
 module.exports = (client, message) => {
     /**
      * Escuta o evento de message
@@ -41,21 +44,39 @@ module.exports = (client, message) => {
     /**
      * Gera um imagem automaticamente
      */
-    if (message.content === '!image') {
-        randomImage(message);
+    if (message.content === '!meme') {
+        randomGif(message);
     }
 
-    if(message.content === 'ta facil?'){
+    if (message.content === 'ta facil?') {
         return message.reply(`Não ta fácil para ninguem!`)
     }
 }
 
 /**
- * Função que gera uma imagem aleatorio do google
+ * Função que gera um gif aleatorio
+ * Buscando por memes
  */
-function randomImage(message) {
+function randomGif(message) {
+    giphy.search("gifs", {q: 'meme'})
+        .then(response => {
+            var totalResponse = response.data.length;
+            var responseIndex = Math.floor(Math.random() * 10 + 1) % totalResponse;
+            var responseFinal = response.data[responseIndex];
+            message.channel.send(`${message.author} Heres Your Gif! :slight_smile:`, {
+                    files: [responseFinal.images.fixed_height.url]
+                }
+            );
+        })
+}
+
+
+/**
+ * Função que gera uma imagem aleatorio do google (cheerio)
+ */
+function randomImageCheerio(message) {
     var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + "meme",
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "food",
         method: "GET",
         headers: {
             "Accept": "text/html",
@@ -63,15 +84,15 @@ function randomImage(message) {
         }
     };
 
-    request(options, function(error, response, responseBody) {
+    request(options, function (error, response, responseBody) {
         if (error) {
             return;
         }
 
-        const $ = cheerio.load(responseBody);
+        $ = cheerio.load(responseBody);
 
-        const links = $(".image a.link");
-        const urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+        var links = $(".image a.link");
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
         console.log(urls);
 
         if (!urls.length) {
@@ -79,6 +100,6 @@ function randomImage(message) {
         }
 
         // Send result
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+        message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
     });
 }
